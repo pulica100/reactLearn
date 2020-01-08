933fb8cd1182264c99514f892a9c358ee19f0ee0
pulica100 <jclossavaro100@hotmail.com>
2020-01-08 11:50:58 -0300

    salvando projeto inacabado de conversor de moeda

---
+++ src/Components/Conversor.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Conversor extends Component {
+
+    render() {
+
+        return (
+
+            <div className='conversor'>
+                <p>Esse é um sapato</p>
+
+
+            </div>
+
+        )
+
+    }
+
+
+
+}
